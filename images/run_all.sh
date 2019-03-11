d19c2458668940de12b4fa591f5e6681b957987c
Hubert Teo <hubert.teo.hk@gmail.com>
2019-03-11 14:06:16 -0700

    one done

---
+++ images/run_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#./run_all_kw.sh raw/Abdullah_Ahmad_Badawi_0001.jpg
+./run_all_kw.sh raw/Angela_Lansbury_0002.jpg
+./run_all_kw.sh raw/Baz_Luhrmann_0001.jpg
+./run_all_kw.sh raw/Bob_Bowlsby_0001.jpg
+./run_all_kw.sh raw/Chakib_Khelil_0001.jpg
+./run_all_kw.sh raw/Hamid_Karzai_0009.jpg
+./run_all_kw.sh raw/Holly_Robinson_Peete_0001.jpg
+./run_all_kw.sh raw/Jean-Marc_de_La_Sabliere_0002.jpg
+./run_all_kw.sh raw/Jia_Qinglin_0001.jpg
+./run_all_kw.sh raw/John_Kerry_0007.jpg
+./run_all_kw.sh raw/Lindy_Ruff_0001.jpg
+./run_all_kw.sh raw/Marcelo_Rios_0005.jpg
+./run_all_kw.sh raw/Mark_Mishkin_0001.jpg
+./run_all_kw.sh raw/Michael_Bolton_0001.jpg
+./run_all_kw.sh raw/Saddam_Hussein_0001.jpg
+./run_all_kw.sh raw/Tab_Turner_0001.jpg
+./run_all_kw.sh raw/Tim_Henman_0012.jpg
+./run_all_kw.sh raw/Tom_Daschle_0011.jpg
+./run_all_kw.sh raw/Wendy_Kennedy_0001.jpg
+./run_all_kw.sh raw/Wen_Jiabao_0003.jpg
