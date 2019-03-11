8b26eb0fd185848bfeb240ba20588d5dab648f4f
Hubert Teo <hubert.teo.hk@gmail.com>
2019-03-11 14:09:30 -0700

    Delete old script

--- images/run_lic_on_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for image in raw/*.jpg; do
-  for kw in {1..20}; do
-    echo "Processing $image, KW = $kw"
-    python ../line-integral-convolution/run_lic.py $image $kw
-  done
-done
