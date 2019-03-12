8ec3381d68443d1ae6c5f9c3d1152e9e554f2dde
Kal Skirata <gael.colas@student.ecp.fr>
2019-03-12 14:58:44 -0700

    baseline test samples

---
+++ baseline/run_baseline_testset.m
@@ -0,0 +1,26 @@
+%% Run the Baseline and the Color from Region Segmentation on the sample test set
+
+% test set image directory
+test_dir = '../images/raw';
+
+
+% get the image filenames
+test_info  = dir( fullfile(test_dir, '*.jpg') );
+test_filenames = fullfile(test_dir, {test_info.name});
+
+% save filenames
+baseline_dir = '../images/baseline';
+segment_dir = '../images/segment';
+baseline_filenames = fullfile(baseline_dir, {test_info.name});
+segment_filenames = fullfile(segment_dir, {test_info.name});
+
+for i=1 : length(test_filenames)
+    % import the images
+    test_image = im2double(imread(test_filenames{i}));
+    % compute the drawing results
+    test_drawing_baseline = blending_baseline_hp(test_image);
+    test_drawing_segment = blending_segment_hp(test_image);
+    % save the results
+    imwrite(test_drawing_baseline, baseline_filenames{i});
+    imwrite(test_drawing_segment, segment_filenames{i});
+end
