18b5a05facce326b03acee07cd13f9729225de19
Hubert Teo <hubert.teo.hk@gmail.com>
2019-03-11 17:52:39 -0700

    python2 was the issue, unfortunately

--- images/run_all_kw.sh
+++ images/run_all_kw.sh
@@ -3,5 +3,5 @@
 image=$1
 for kw in {2..20}; do
 echo "Processing $image, KW = $kw"
-python ../line-integral-convolution/run_lic.py $image $kw
+python3 ../line-integral-convolution/run_lic.py $image $kw
 done
